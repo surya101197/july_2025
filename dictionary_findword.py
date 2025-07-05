ec68cdf4b1131965abeecbd04239d21fac1e80c7
surya101197 <suryanarayana97@yahoo.com>
2025-07-05 15:23:33 +0530

    dictionary_findword.py

--- dictionary_findword.py
+++ dictionary_findword.py
@@ -9,10 +9,10 @@ while True:
     user_1=input("Choose an option: ")
 
     if user_1=="1":
-        choose_=input("Come on add a word you fucker: ").title()
-        choose_1=input("Come on fucker enter its meaning: ").title()
+        choose_=input("Come on add a word: ").title()
+        choose_1=input("Come on enter its meaning: ").title()
         dictionary[choose_]=choose_1
-        print("word added and meaning added bey")
+        print("word added and meaning added")
     elif user_1=="2":
         find_meaning=input("enter word: ")
         if find_meaning in dictionary:
@@ -26,11 +26,11 @@ while True:
             dictionary[update_word_and_meaning]=update_meaning
             print("word updated")
         elif update_word_and_meaning in dictionary:
-            print("word already existing ra erri puka")
+            print("word already existing")
     elif user_1=="4":
         print(dictionary)
     elif user_1=="5":
-        print("Dengey")
+        print("bye")
         break
 
 
